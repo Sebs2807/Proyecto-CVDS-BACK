131b8c330aa776875576982ad5147a548eb38b38
santi-123 <santiago.diaz-r@mail.esculaing.edu.co>
2024-12-12 07:09:50 -0500

    Correccion de error al momento de autenticar token por tiempo de expiracion

---
+++ Library/src/main/java/edu/eci/cvds/library/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package edu.eci.cvds.library.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("BearerAuth",
+                    new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("Ingrese el token en el formato: Bearer {token}")
+                ));
+    }
+}
